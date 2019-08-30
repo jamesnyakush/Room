73b2eed70580fa424e6630ff8150a0ce8c2217a2
jamesnyakush <emvesttech@gmail.com>
2019-08-30 17:36:14 +0300

    rere

--- app/src/main/java/com/example/room/ui/AddFrag.kt
+++ app/src/main/java/com/example/room/ui/AddFrag.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.example.room.R
+import com.example.room.db.Note
+import com.example.room.db.NoteDatabase
+import kotlinx.android.synthetic.main.fragment_add.*
 
 class AddFrag : Fragment() {
 
@@ -16,6 +20,24 @@ class AddFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        save.setOnClickListener {
+            val title = title.text.toString().trim()
+            val noti = notify.text.toString().trim()
+
+            if (title.isEmpty() && noti.isEmpty()){
+                Toast.makeText(context,"all fields required",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            val  note = Note(title,noti)
+            NoteDatabase(activity!!).getNoteDao().addNote()
+        }
+
     }
 
 
